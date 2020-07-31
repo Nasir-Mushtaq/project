774de3acad0fd953deb981e245f3804e6e22cbd4
Nasir-Mushtaq <nasir.mushtaq@gmail.com>
2020-07-31 12:06:23 +1000

    Added sample cards

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function addButtonClicked() {
 
 function newTask(name, description, assignedTo, dueDate, startTime, finishTime) {
 const html = `
-<div class="card time">
+<div class="card time pt-0">
   <div class="card-body">
   <h5 class="card-title">${name} - ${startTime}</h5>
   <h6 class="card-subtitle >${description}</h6>
@@ -33,5 +33,65 @@ const html = `
 const taskElement = document.createRange().createContextualFragment(html);
 taskContainer.append(taskElement);
 }
-
+newTask(
+  name = "Daily opening",
+  description = "",
+  assignedTo = "Nasir",
+  dueDate = "",
+  startTime = "9 AM",
+  finishTime = "",
+);
+newTask(
+  name = "Async task",
+  description = "",
+  assignedTo = "Nasir",
+  dueDate = "",
+  startTime = "9:15 AM",
+  finishTime = "",
+);
+newTask(
+  name = "Zoom with Anindha",
+  description = "",
+  assignedTo = "Nasir",
+  dueDate = "",
+  startTime = "11 AM",
+  finishTime = "",
+);
+newTask(
+  name = "Lunch",
+  description = "",
+  assignedTo = "Nasir",
+  dueDate = "",
+  startTime = "12:30 PM",
+  finishTime = "",
+);
+newTask(
+  name = "Async task",
+  description = "",
+  assignedTo = "Nasir",
+  dueDate = "",
+  startTime = "1:00 PM",
+  finishTime = "",
+);newTask(
+  name = "Zoom with Anindha",
+  description = "",
+  assignedTo = "Nasir",
+  dueDate = "",
+  startTime = "3 PM",
+  finishTime = "",
+);newTask(
+  name = "Async task",
+  description = "",
+  assignedTo = "Nasir",
+  dueDate = "",
+  startTime = "4 PM",
+  finishTime = "",
+);newTask(
+  name = "Daily reflecion",
+  description = "",
+  assignedTo = "Nasir",
+  dueDate = "",
+  startTime = "4:45 pM",
+  finishTime = "",
+);
 
